01f7640637003b509eacb3549b1adad6a3ba779c
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-22 21:36:53 +0530

    Update Hero.jsx

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -36,6 +36,7 @@ const Hero = () => (
       >
         <div className="absolute w-full h-[300px]
         hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
+
           <img
           src="/cover.png"
           alt="cover"
