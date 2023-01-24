a2b2087181463dc739d2db12a3537f8016921f4a
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-25 00:48:08 +0530

    updated

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,9 +1,14 @@
 'use client';
+import { motion } from "framer-motion";
+import styles from '../styles';
+import { fadeIn } from '../utils/motion';
 
-const ExploreCard = () => (
-  <div>
-    Explore Card
-  </div>
+
+const ExploreCard = ({id, imgUrl, title, index, active, handleClick}) => (
+  <motion.div variants={fadeIn('right', 'spring', index *0.5)}
+  >
+   Explore Card
+  </motion.div> 
 );
 
 export default ExploreCard;
