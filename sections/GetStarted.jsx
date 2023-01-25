333dfe18135fcbc2105c84adf7d4f04f4ceef8cd
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-26 00:22:16 +0530

    updated

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,8 +1,34 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { startingFeatures } from '../constants';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { StartSteps, TitleText, TypingText } from '../components';
+
+
 const GetStarted = () => (
-  <section>
-    Get Started Section
+  <section className={`${styles.paddings} relative  z-10`}
+  >
+    <motion.div 
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport="{{once: 'false', amount: 0.25 }}"
+    className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+    <motion.div variants={planetVariants('right')}
+    className={`flex-1 ${styles.flexCenter}`}
+    >
+      <img 
+      src='/get-started.png'
+      alt='planet'
+      className='w-[90%] h-[90%] object-contain'
+      />
+
+    </motion.div>
+    </motion.div>
   </section>
 );
 
