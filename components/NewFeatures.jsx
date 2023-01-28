33ace80f998767db7a944de9bfdfedb74e889d5a
Siddharth verma <990.siddharthverma@gmail.com>
2023-01-29 02:56:54 +0530

    feat: updated

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -9,7 +9,10 @@ const NewFeatures = ({imgUrl, title, subtitle}) => (
        />
       </div>
       <h1 className="mt-[26px] font-bold text-[24px]
-      leading-[30px] text-white"> Title {title} </h1>
+      leading-[30px] text-"> Title {title} </h1>
+      <p className="flex-1 mt-[16px] font-normal text-[18px] text-white leading-[32px]">
+      {subtitle}  
+      </p>
   </div>
 );
 
