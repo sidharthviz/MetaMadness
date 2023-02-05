8292f44a3d66ad4e83af553da413f675c4a2a5c7
Siddharth verma <990.siddharthverma@gmail.com>
2023-02-05 23:00:27 +0530

    chore: updated

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -3,10 +3,9 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { TitleText, TypingText, InsightCard } from '../components';
-import { staggerContainer, fadeIn,  } from '../utils/motion';
 import { insights } from '../constants';
-
+import { staggerContainer } from '../utils/motion';
+import { InsightCard, TitleText, TypingText } from '../components';
 
 const Insights = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -15,18 +14,16 @@ const Insights = () => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex  flex-col`}
-      >
-        <TypingText title=" | Insight"
-        textStyles="text-center" />
-        <TitleText title="Insight about Metaverse"
-        textStyles="text-center" />
-         <div className='mt[50px] flex flex-col gap[30px]'>
-          {insights.map((insight, index) => {
-            <InsightCard key={`insight-${index}`} {...insight} index={insight+1} />
-          })}
-         </div>
-      </motion.div>
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| Insight" textStyles="text-center" />
+      <TitleText title={<>Metaverse Insight</>} textStyles="text-center" />
+      <div className="mt-[50px] flex flex-col gap-[30px]">
+        {insights.map((item, index) => (
+          <InsightCard key={`insight-${index}`} {...item} index={index + 1} />
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
